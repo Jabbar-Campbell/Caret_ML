9d03f49132ec097d5f50573b59f62dbc29ca2c64
Jabbar Campbell <Jabbar_Campbell@hotmail.com>
2023-08-15 14:03:11 -0400

    specificity sensitivity defined

---
+++ Machine learning with caret.R
@@ -0,0 +1,154 @@
+install.packages('dslabs')
+install.packages("caret")
+install.packages('heights')
+library(dslabs)
+library(tidyverse)
+library(caret)
+
+
+# x_Train x_Train x_Train x_Train x_Train | y_hat
+# x_Train x_Train x_Train x_Train x_Train | y_hat
+# x_Train x_Train x_Train x_Train x_Train | y_hat
+# x_Train x_Train x_Train x_Train x_Train | y_hat
+# x_Train x_Train x_Train x_Train x_Train | y_hat
+# x_Train x_Train x_Train x_Train x_Train | y_hat
+# x_Train x_Train x_Train x_Train x_Train | y_hat
+# x_Train x_Train x_Train x_Train x_Train | y_hat
+# x_Train x_Train x_Train x_Train x_Train | y_hat
+# x_Train x_Train x_Train x_Train x_Train | y_hat
+# x_Train x_Train x_Train x_Train x_Train | y_hat
+# x_Train x_Train x_Train x_Train x_Train | y_hat
+-------------------------------------------------
+# X_test  X_test  X_test  X_test  X_test  | y_hat
+# X_test  X_test  X_test  X_test  X_test  | y_hat
+# X_test  X_test  X_test  X_test  X_test  | y_hat
+# X_test  X_test  X_test  X_test  X_test  | y_hat
+# X_test  X_test  X_test  X_test  X_test  | y_hat
+# X_test  X_test  X_test  X_test  X_test  | y_hat
+# X_test  X_test  X_test  X_test  X_test  | y_hat
+# X_test  X_test  X_test  X_test  X_test  | y_hat
+# X_test  X_test  X_test  X_test  X_test  | y_hat
+# X_test  X_test  X_test  X_test  X_test  | y_hat
+# X_test  X_test  X_test  X_test  X_test  | y_hat
+# X_test  X_test  X_test  X_test  X_test  | y_hat
+
+
+data(heights)
+
+
+# Define the outcome and the predictors
+
+x<- heights$height #what we want to use to drive out prediction
+y<- heights$sex  #what we're trying to predict
+length(y)
+
+# Generate training and test data First we randomly sample 
+# our data and giving and mark those positions with and index number
+# this is done with the createDataPartition. Those indexs are assigned as Test data and everthing else 
+# we set as  to training data
+ 
+set.seed(2007)
+test_index<-createDataPartition(y,times = 1, p = .05, list = TRUE)
+x_Test<-heights[test_index,]# a subset index's
+x_Train<-heights[-test_index,]# everything else
+
+
+# sample the sex column taking the same number of samples as the index
+y_hat<-sample(x = c("Male","Female"), size = length(test_index), replace = TRUE)
+
+
+
+# we can compare these to methods of sampling and check for accuracy
+# numbers of times the values is the same
+mean(y_hat == x_Test$sex)
+
+
+# We see on average the heights are close
+# between sexs
+heights %>% 
+  group_by(sex) %>% 
+  summarize(mean(height),sd(height) )
+
+
+
+
+# without partitioning anything
+# lets make a rule(model) that says anything greater
+# than 62 is male and save the result as y_hat2
+# turns out this is more accurate than our sampling method
+y_hat2 <- ifelse(x > 62, "Male", "Female")
+mean(y == y_hat2)
+
+
+
+
+
+
+#set a list of cutoffs(rules) and pick which one
+cutoff<-seq(61,70)
+# like an lapply for every element in cutoff perform
+# the function that says if the height of out training
+# data is great then the cutoff value your male 
+# well get a series of tables one for each cut off
+# each is compared to the training data for accuracy
+
+lapply(cutoff,function(x){
+  y_hat3 <- ifelse(x_Train$height > x, "Male" , "Female")   
+    #factor(levels = levels(x_Test$sex))
+  mean(y_hat3 == x_Train$sex)
+  
+} )
+
+
+
+# BIAS IN THE TRAINING SET IS DETRIMENTAL
+# A table of predicted vs actual data gives us valuable info 
+# we sampled  sex data based on indices and compared it to a sample(data) function on sex  
+# of the predicted female 7 where female the rest where actually males
+# of the male category 5 where female and 24 where actually males
+# this shows that males are correctly estimated more than females are
+table(predicted_data = y_hat, actual_data = x_Test$sex)
+
+
+
+
+
+
+######################################### SENSITIVITY AND sPECIFICITY############################################## 
+# 1 = positive
+# 0 = negative
+# SENSITIVITY is the ability to predict a positive outcome
+# when the actual outcome was also positive ie
+# y_hat == 1 when y_test or y _train == 1
+# its a ratio of the true positives 
+# divided by the true positive plus False Negatives
+# SPECIFICTY 
+# if we can go the other way and predict negative cases
+# as well then this speaks to specificity
+# y_hat == 0 when y_test or y_train == 0
+
+# a confusion matrix expresses this!
+
+confusionMatrix(data = factor(y_hat), reference = factor(x_Test$sex))
+
+data("heights")
+data(heights)
+heights
+class(heights)
+str(heights)
+dim(heights)
+heights[777,1]
+summary(heights)
+which('sex' == "female" && heights$height > 78)
+heights
+dplyr::filter(heights, heights$height > 78)
+library dplyr::where
+ 
+
+ncol(df$train$images)
+
+df<-read_mnist()
+str(df)
+60000*784
+
+
