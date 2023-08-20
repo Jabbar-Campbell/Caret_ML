76c5ddd4c73fdb351af4427f4dfd88e310b943c6
Jabbar Campbell <Jabbar_Campbell@hotmail.com>
2023-08-20 14:02:45 -0400

    here we see how means can give probability in a Bivariate situation

--- BAYES FU.R
+++ BAYES FU.R
@@ -1,14 +1,17 @@
-#####################BAYES THEOROM  P(A|B)  =  P(B|A)    *      (P(A) / P(B))
+#####################BAYES THEOROM  P(A|B)  =  P(B|A)    *      (P(A) / P(B))#################
 
-#         Since
-#   Healthy + Disease =1
+#            Since
+#   Prevalence = p(Disease) = .02
+#         its a must that
+#   p(Healthy) + p(Disease) =1
+
 #            and
 #### p(test - | Healthy) = .90
 #### p(test + | Disease) = .85
 #              then
 ####### p(test + | Healthy) = .10
 ####### p(test - | Disease) = .15
-
+####### p(Healthy)= .98
 
 
 
@@ -29,7 +32,7 @@ table(disease)
 
 
 
-############################### TESTED DATA with % RELIABILITY 
+############################### TESTED DATA with % RELIABILITY #################################### 
 # 0  means the test negative
 # P(test - | healthy)  = .90
 # set up zeros and ones amounting to the zeros indicating healthy folks  and assign to a 
@@ -67,4 +70,56 @@ mean(disease[test==1])
 mean(disease[test==1])
 
 
-.149+.003
+
+
+
+
+
+
+###############################  THE POWER of QUANTILES ###########################
+library(dslabs)
+data("heights")
+
+# p(Male|Height) = p(height|male) p(male)/p(height)
+# or
+heights %>% 
+  mutate(height = round(height)) %>% 
+  group_by(height) %>% #if we didn't do this it would just be the p(Male)
+  summarize(p = mean(sex == "Male")) %>% 
+qplot(height,p,data=.)
+
+# heights has alot of variability in terms sex at the smaller heights
+# to get around this we can cut the data into 
+# 10% bins and see what the value is at that cutoff
+heights$height %>% 
+cut(.,quantile(.,seq(0,1,.1)), include.lowest = TRUE)
+
+
+# Quantiles are the lines that divided data in equally sized groups
+ps<-seq(0,1,.1)
+quantile(heights$height, ps) 
+
+heights %>% 
+  mutate(g = cut(height, quantile(height, ps), include.lowest = TRUE))%>% 
+  group_by(g) %>%  # now the data is grouped by bin instead of each height value
+  summarize(p = mean(sex =="Male"), height = mean(height)) %>% 
+  qplot(height,p,data = .)
+
+# there is a Normal Distribution
+#              Bivariate Normal Distribution
+#                           multivariate Normal Distribution
+
+sigma<-9*matrix(c(1,0.5,0.5,1),2,2) # first we make a matrix of covariance
+dat<-MASS::mvrnorm(n=10000,c(69,69),sigma) %>%  #combine sample number desired, mean of the variables and the variance
+  data.frame() %>% setNames(c("x","y")) # x and y are related to each other
+
+
+ps<-seq(0,1,0.1)
+
+dat %>% 
+  mutate(g = cut(x, quantile(x, ps), include.lowest = TRUE)) %>%
+  group_by(g) %>%
+  summarize(y = mean(y), x = mean(x)) %>% 
+  qplot(x,y,data = .)
+
+ 
